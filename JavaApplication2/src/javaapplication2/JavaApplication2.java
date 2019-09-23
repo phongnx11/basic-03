5013ee309b8e02852a6f77454c3bae006d0835b6
phongnx11 <55699585+phongnx11@users.noreply.github.com>
2019-09-23 20:49:04 +0700

    Lesson 1

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author admin
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float a =3
+                ;
+        float b = 6;
+       float x = -b/a;
+      if(a==0&&b!=0){
+          System.out.println("pt vo nghiem");
+          if(a!=0){
+              System.out.println("ptconghiemla");
+              x=-b/a;
+              System.out.println(x);
+          }
+      }
+    }
+
+}
---
+++ action1/src/action1/Action1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action1;
+
+/**
+ *
+ * @author admin
+ */
+public class Action1 {
+
+    /**
+     * @param args the command line argume 
+    public static void main(String[] args) {
+        int SoNguyen = 7;
+        // Datatype 
+        boolean bl = true;
+        char c = 'a';
+        double d = 123.565;
+
+        float f = 0.5f;
+        int A = 7;
+        int B = 2;
+        int ketqua = A / B;
+        int phandu = A % B;
+        System.out.println("day la A" + A);
+        System.out.println(B);
+        System.out.println(ketqua);
+        System.out.println(phandu);
+    }
+
+}
