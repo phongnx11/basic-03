9f418eb4efad8f24126fa774bc1f3b60e34a3bc5
phongnx11 <55699585+phongnx11@users.noreply.github.com>
2019-09-23 23:12:37 +0700

    Add files via upload

---
+++ btvn1/src/btvn1/Btvn1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btvn1;
+
+/**
+ *
+ * @author admin
+ */
+public class Btvn1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code apppublic static void main(String[] args) {
+        float a = 11;
+        float b = 8;
+        float tong;
+        float tich;
+        float hieu;
+        float thuong;
+        System.out.println("Tong la :");
+        tong = a + b;
+        System.out.println(tong);
+        System.out.println("Hieu la");
+        hieu = a - b;
+        System.out.println(hieu);
+        System.out.println("tich la");
+        tich = a * b;
+        System.out.println(tich);
+        System.out.println("Thuong la");
+        thuong = a / b;
+        System.out.println(thuong);
+    }
+    
+}
