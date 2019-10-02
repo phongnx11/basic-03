c923bbc7e38934568641fead8ddfe32184903092
phongnx11 <55699585+phongnx11@users.noreply.github.com>
2019-10-02 18:11:01 +0700

    HW3

--- BT8/src/bt8/BT8.java
+++ BT8/src/bt8/BT8.java
@@ -18,34 +18,28 @@ public class BT8 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
-        int a[] = new int[100];
         int n;
-        n = sc2.nextInt();
-        System.out.println("kich thuoc cua ma tran la   " + n);
-        System.out.println("nhap cac phan tu cua day");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("a[" + i + "]");
-            a[i] = sc.nextInt();
+        System.out.println("nhap cac kich thuoc cua mang ");
+n = sc.nextInt();
+        int a[][] = new int[n][n];
+
+        
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+                System.out.println("a[" + (i + 1) + "]" + "[" + (j + 1) + "}");
+            }
 
         }
-        for (int i = 0; i < 3; i++) {
-            System.out.print("  " + a[i]);
-            System.out.print("  " + a[i]);
-            System.out.println("  " + a[i]);
+        System.out.println("ma tran a : ");
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j]+"     ");
+                
+            }
+             System.out.print("\n");
+            
         }
-        for (int i = 3; i < 7; i++) {
-             System.out.print("  " + a[i]);
-            System.out.print("  " + a[i]);
-            System.out.println("  " + a[i]);
-        }
-
-        for (int i = 7; i < 10; i++) {
-             System.out.print("  " + a[i]);
-            System.out.print("  " + a[i]);
-            System.out.println("  " + a[i]);
-        }
-
     }
-
 }
